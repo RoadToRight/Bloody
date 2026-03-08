e873357f86364a7b4c9f6b0fdacaa877ef294094
RoadToRight <syedsameershah2008@gmail.com>
2026-03-07 18:49:38 -0800

    create product

--- Backend/controllers/productControllers.js
+++ Backend/controllers/productControllers.js
@@ -1,18 +1,48 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 import ProductModel from "../models/productModel.js";
+import { v2 as cloudinary } from "cloudinary";
 
 export const createProduct = catchAsyncErrors(async (req, res, next) => {
-    const { name, images, price, variants, rating, productDetails, specifications, socialLinks, tags, properties, software, handle } = req.body;
-
-    if (!name || !price || !productDetails || !specifications || !software || !handle) {
-        return next(new ErrorHandler("Please Fill Required Fields To Create Product"))
+    const { name, price, variants, rating, productDetails, specifications, socialLinks, tags, properties, software, handle } = req.body;
+    let images = [];
+    let uploadedImages = [];
+    if (!name || price == null || !productDetails || !specifications || !software || !handle) {
+        return next(new ErrorHandler("Please Fill Required Fields To Create Product", 400))
     }
+    if (req.files && req.files.images) {
+        images = Array.isArray(req.files.images) ? req.files.images : [req.files.images];
+    }
+    if (req.files && req.files.images && req.files.images.length > 0) {
 
-    const Product = await ProductModel.create({ name, images, price, variants, rating, productDetails, specifications, socialLinks, tags, properties, software, handle })
+        uploadedImages = await Promise.all(images.map(async (img) => {
+            const rawuploadedImages = await cloudinary.uploader.upload(img.tempFilePath, {
+                folder: "Bloody/products"
+            });
+            return { url: rawuploadedImages?.secure_url, public_id: rawuploadedImages?.public_id }
+        })
+        )
 
-    if(Product){
-        
+    }
+    const product = await ProductModel.create({ name, uploadedImages, price, variants, rating, productDetails, specifications, socialLinks, tags, properties, software, handle })
+
+
+    if (product) {
+        res.status(201).json({
+            success: true,
+            product,
+            message: "Product Created Successfully",
+        })
+    } else {
+        if (uploadedImages && uploadedImages.length > 0) {
+            await Promise.all(
+                uploadedImages.map(async (img) => {
+                    await cloudinary.uploader.destroy(img.public_id)
+                })
+            )
+        }
+
+        return next(new ErrorHandler("Failed To Create Product", 400))
     }
 })
 export const getProduct = catchAsyncErrors(async (req, res, next) => {
--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,9 @@
 import app from "./app.js";
+import { v2 as cloudinary } from "cloudinary";
 
+cloudinary.config({
+    api_key: process.env.CLOUDINARY_CLIENT_API,
+    api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLIENT_NAME
+})
 app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.PORT}`))
