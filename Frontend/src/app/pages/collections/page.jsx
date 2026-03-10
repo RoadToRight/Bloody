7ec167d46dba4c35aed31f627b03facc7673d65f
RoadToRight <syedsameershah2008@gmail.com>
2026-03-10 02:06:28 -0700

    filter box

--- Frontend/src/app/pages/collections/page.jsx
+++ Frontend/src/app/pages/collections/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import {
     Box,
@@ -10,8 +11,34 @@ import {
     MenuItem,
 } from "@mui/material";
 import ProductCard from "@/components/ProductCard";
-
+import { FaMinus } from "react-icons/fa6";
+import FilterBox from "@/components/client/FilterBox";
 const Collections = () => {
+
+    const FiltersData = [
+        {
+            title: "SWITCH",
+            options: ["Mecha-Like (3)", "Mechanical (BLMS) (7)", "Optical (AT) (4)"]
+        },
+        {
+            title: "LIGHTING",
+            options: ["Neon (4)", "RGB (8)"]
+        },
+        {
+            title: "KEYS",
+            options: ["98-100 (5)", "104-113 (5)", "60-87 (TKL) (5)"]
+        },
+        {
+            title: "GHOST KEYS",
+            options: ["Full N-Key Rollover (11)", "Multi N-Key Rollover (4)"]
+        },
+        {
+            title: "AVAILABILITY",
+            options: ["In Stock (12)", "Out Of Stock (3)"]
+        },
+    ]
+
+
     return (
         <Box
             sx={{
@@ -21,64 +48,36 @@ const Collections = () => {
                 py: 5,
             }}
         >
-            <Box className={"container"} sx={{ display: "flex" }} spacing={4}>
+            <Grid container spacing={4} sx={{ display: "flex" }}>
                 {/* LEFT FILTER SECTION */}
-                <Grid item xs={12} md={3}>
-                    {/* SWITCH */}
-                    <Box sx={{ mb: 4 }}>
-                        <Typography sx={{ fontWeight: 700, mb: 2 }}>SWITCH</Typography>
 
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="Mecha-Like (3)" />
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="Mechanical (BLMS) (7)" />
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="Optical (AT) (4)" />
+                {/* <Grid size={{ xs: 12, md: 3 }}>
+                    {FiltersData.map((filter, index) => (
+                        <Box key={index} sx={{ mb: 4 }}>
+                            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: "4px" }} onClick={(e) => handleAccordian(e)}>
+                                <Typography sx={{ fontWeight: 700 }}>
+                                    {filter.title}
+                                </Typography>
+                                <FaMinus />
 
-                        <Divider sx={{ borderColor: "red", mt: 2 }} />
-                    </Box>
+                            </Box>
 
-                    {/* LIGHTING */}
-                    <Box sx={{ mb: 4 }}>
-                        <Typography sx={{ fontWeight: 700, mb: 2 }}>LIGHTING</Typography>
+                            {filter.options.map((option, i) => (
+                                <FormControlLabel
+                                    key={i}
+                                    control={<Checkbox sx={{ color: "#fff" }} />}
+                                    label={option}
+                                />
+                            ))}
 
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="Neon (4)" />
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="RGB (8)" />
-
-                        <Divider sx={{ borderColor: "red", mt: 2 }} />
-                    </Box>
-
-                    {/* KEYS */}
-                    <Box sx={{ mb: 4 }}>
-                        <Typography sx={{ fontWeight: 700, mb: 2 }}>KEYS</Typography>
-
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="98-100 (5)" />
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="104-113 (5)" />
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="60-87 (TKL) (5)" />
-
-                        <Divider sx={{ borderColor: "red", mt: 2 }} />
-                    </Box>
-
-                    {/* GHOST KEYS */}
-                    <Box sx={{ mb: 4 }}>
-                        <Typography sx={{ fontWeight: 700, mb: 2 }}>GHOST KEYS</Typography>
-
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="Full N-Key Rollover (11)" />
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="Multi N-Key Rollover (4)" />
-
-                        <Divider sx={{ borderColor: "red", mt: 2 }} />
-                    </Box>
-
-                    {/* AVAILABILITY */}
-                    <Box sx={{ mb: 4 }}>
-                        <Typography sx={{ fontWeight: 700, mb: 2 }}>AVAILABILITY</Typography>
-
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="In Stock (12)" />
-                        <FormControlLabel control={<Checkbox sx={{ color: "#fff" }} />} label="Out Of Stock (3)" />
-
-                        <Divider sx={{ borderColor: "red", mt: 2 }} />
-                    </Box>
-                </Grid>
+                            <Divider sx={{ borderColor: "red", mt: 2 }} />
+                        </Box>
+                    ))}
+                </Grid> */}
+                <FilterBox FiltersData={FiltersData}/>
 
                 {/* RIGHT PRODUCTS SECTION */}
-                <Grid item xs={12} md={9}>
+                <Grid size={{ xs: 12, md: 9 }}>
                     {/* SORT */}
                     <Box
                         sx={{
@@ -108,32 +107,32 @@ const Collections = () => {
 
                     {/* PRODUCT GRID */}
                     <Grid container spacing={3}>
-                        <Grid item xs={12} sm={6} md={4}>
+                        <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                             <ProductCard />
                         </Grid>
 
-                        <Grid item xs={12} sm={6} md={4}>
+                        <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                             <ProductCard />
                         </Grid>
 
-                        <Grid item xs={12} sm={6} md={4}>
+                        <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                             <ProductCard />
                         </Grid>
 
-                        <Grid item xs={12} sm={6} md={4}>
+                        <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                             <ProductCard />
                         </Grid>
 
-                        <Grid item xs={12} sm={6} md={4}>
+                        <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                             <ProductCard />
                         </Grid>
 
-                        <Grid item xs={12} sm={6} md={4}>
+                        <Grid size={{ xs: 12, sm: 6, md: 4 }}>
                             <ProductCard />
                         </Grid>
                     </Grid>
                 </Grid>
-            </Box>
+            </Grid>
         </Box>
     );
 };
---
+++ Frontend/src/components/client/FilterBox.jsx
@@ -0,0 +1,45 @@
+"use client"
+import { Box, Checkbox, Divider, FormControlLabel, Grid, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { FaMinus } from 'react-icons/fa6'
+
+const FilterBox = ({ FiltersData }) => {
+
+    const [activeAccordian, setactiveAccordian] = useState(FiltersData)
+
+    const handleAccordian = (index) => {
+        setactiveAccordian((prev) => ({
+            ...prev, [index]: !prev[index]
+        }))
+    }
+
+    
+
+    return (
+        <Grid size={{ xs: 12, md: 3 }}>
+            {FiltersData.map((filter, index) => (
+                <Box key={index} sx={{ mb: 4 }}>
+                    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: "4px", cursor: "pointer" }} onClick={() => handleAccordian(index)}>
+                        <Typography sx={{ fontWeight: 700 }}>
+                            {filter.title}
+                        </Typography>
+                        <FaMinus />
+
+                    </Box>
+
+                    {activeAccordian[index] && filter.options.map((option, i) => (
+                        <FormControlLabel
+                            key={i}
+                            control={<Checkbox sx={{ color: "#fff" }} />}
+                            label={option}
+                        />
+                    ))}
+
+                    <Divider sx={{ borderColor: "red", mt: 2 }} />
+                </Box>
+            ))}
+        </Grid>
+    )
+}
+
+export default FilterBox
